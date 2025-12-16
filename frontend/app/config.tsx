0883f1669bb0f856cf29b792d1216f83f45db6d5
Rara Nur Annisa <nuranisarara02@gmail.com>
2025-12-16 17:54:21 +0700

    update

---
+++ frontend/app/config.tsx
@@ -0,0 +1,62 @@
+export const appInfo = {
+  "appName": "Digilibz",
+  "tagLine": "Borrow a book more easier."
+}
+
+export const assets = {
+  "logoUrl": "https://www.shadcnblocks.com/images/block/block-1.svg",
+  "bgHero": "/assets/bg.svg",
+}
+
+export const recomendations = [
+  { name: 'Bumi - Tere Liye', href: '/collections?search=bumi - tere liye' },
+  { name: 'The Art Of War', href: '/collections?search=the art of war' },
+  { name: 'Harry Potter', href: '/collections?search=harry potter' },
+];
+
+export const footerContents = {
+  sections: [
+    {
+      title: 'Product',
+      links: [
+        { name: 'Overview', href: '/overview' },
+        { name: 'Pricing', href: '/pricing' },
+        { name: 'Marketplace', href: '/marketplace' },
+        { name: 'Features', href: '/features' },
+        { name: 'Integrations', href: '/integrations' },
+        { name: 'Pricing', href: '/pricing' },
+      ],
+    },
+    {
+      title: 'Company',
+      links: [
+        { name: 'About', href: '/about' },
+        { name: 'Team', href: '/team' },
+        { name: 'Blog', href: '/blog' },
+        { name: 'Careers', href: '/careers' },
+        { name: 'Contact', href: '/contact' },
+        { name: 'Privacy', href: '/privacy' },
+      ],
+    },
+    {
+      title: 'Resources',
+      links: [
+        { name: 'Help', href: '/help' },
+        { name: 'Sales', href: '/sales' },
+        { name: 'Advertise', href: '/advertise' },
+      ],
+    },
+    {
+      title: 'Social',
+      links: [
+        { name: 'Twitter', href: '/twitter' },
+        { name: 'Instagram', href: '/instagram' },
+        { name: 'LinkedIn', href: '/linkedin' },
+      ],
+    },
+  ],
+  bottomContent: [
+    { name: 'Terms and Conditions', href: '/terms-and-conditions' },
+    { name: 'Privacy Policy', href: '/privacy-policy' },
+  ],
+}
