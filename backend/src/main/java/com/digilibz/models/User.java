a01ce82717268ecef2b53fd751d5a2438da8768e
Intannrna <intannrna32@gmail.com>
2025-12-16 20:43:43 +0700

    update

---
+++ backend/src/main/java/com/digilibz/models/User.java
@@ -0,0 +1,40 @@
+package com.digilibz.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "email"),
+                @UniqueConstraint(columnNames = "phone")
+        })
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(unique = true)
+    private String phone;
+
+    public enum Role {
+        ADMIN,
+        USER
+    }
+
+    public User() {}
+}
